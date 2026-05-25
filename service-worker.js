ecb142be8d3614cfa321c5c30ee92d7b984f6b0f
pusiska <tankerzol@mail.ru>
2026-05-25 15:13:49 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'my-books-epub-reader-v515';
+const CACHE_NAME = 'my-books-epub-reader-v516';
 
 const APP_SHELL = [
   './',
   './index.html',
-  './index.html?v=515',
+  './index.html?v=516',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
@@ -38,7 +38,7 @@ function getCacheKey(request) {
 }
 
 async function getOfflineShell() {
-  return await caches.match('./index.html?v=515') || await caches.match('./index.html') || await caches.match('./');
+  return await caches.match('./index.html?v=516') || await caches.match('./index.html') || await caches.match('./');
 }
 
 self.addEventListener('install', event => {
